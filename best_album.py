c7897f06ba603236e427c0dab2a333f85ea64b06
blackreo123 <blackreo123@naver.com>
2021-07-15 10:39:41 +0900

    best_album solved

--- best_album.py
+++ best_album.py
@@ -1,39 +1,45 @@
-import operator
+
+
 def solution(genres, plays):
     answer = []
-    각각플레이수 = []
-    장르별총플레이수 = {}
-    전체순서 = []
-    # 장르별 순위를 정한다
-    for 번째 in range(0,len(genres)):
-        각각플레이수.append({번째 : {genres[번째] : plays[번째]}})
-        if genres[번째] in 장르별총플레이수.keys():
-            장르별총플레이수[genres[번째]] = 장르별총플레이수[genres[번째]] + plays[번째]
-        else:
-            장르별총플레이수[genres[번째]] = plays[번째]
-    플레이수_많은순_장르별총플레이수 = sorted(장르별총플레이수.items(), key=operator.itemgetter(1), reverse=1)
-    플레이수_많은순_장르 = []
-    for i in 플레이수_많은순_장르별총플레이수:
-        플레이수_많은순_장르.append(i[0])
-    for i in range(len(각각플레이수)):
-        for j in range(len(각각플레이수) - 1 ,i, -1):
-            if list(list(각각플레이수[j].items())[0][1].values())[0] > list(list(각각플레이수[j-1].items())[0][1].values())[0]:
-                각각플레이수[j],각각플레이수[j-1] = 각각플레이수[j-1], 각각플레이수[j] 
+
+    # 곡 별 재생 수 리스트
+    list_a = []
+    for i in range(0,len(genres)):
+       list_a.append({"num" : i, "genre" : genres[i], "play" : plays[i]}) 
+    list_a = sorted(list_a, key = lambda item: (-item["play"], item["num"]))
     
-    for 장르 in 플레이수_많은순_장르별총플레이수:
-        for i in range(0,len(각각플레이수)):
-            
-        # 장르가 같은거 뽑기
-            if 장르[0] in list(각각플레이수[i].values())[0].keys():
-                전체순서.append(각각플레이수[i])
-    print(플레이수_많은순_장르)
-    print(전체순서[0])
-    # 나열한다
-    # for i in 플레이수_많은순_장르:
-    #     for j in 전체순서:
-    #         if 
-        # answer.append(list(i.keys())[0])
-    # print(answer)
+    # 장르 종류만 거르기
+    genre = {}
+    for i in genres:
+        genre[i] = 0
+    
+    # 장르별 총 플레이 수
+    for i in genre:
+        for j in list_a:
+            if i == j["genre"]:
+               genre[i] =  genre[i]+j["play"]
+     
+    # 장르별 순위
+    sorted_genre = sorted(genre.items(), key = lambda item: -item[1])
+    # print(sorted_genre)
+    
+    # 장르별 곡 묶기 {장르 :[고유번호]}
+    temp = {}
+    for i in sorted_genre:
+        temp_list = []
+        for j in list_a:
+            if i[0] == j["genre"]:
+                temp_list.append(j["num"])
+                if len(temp_list) == 2:
+                    break
+        temp[i[0]] = temp_list
+
+    # 마지막 배열에 담기
+    for i in list(temp.values()):
+        for j in range(len(i)):
+            answer.append(i[j])
+    
     return answer
 
 
